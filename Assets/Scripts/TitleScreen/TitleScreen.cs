df4fd37d0c0b3a772200a1e2d33e8d5cae3389e9
jimmy <jimmy_le_@hotmail.com>
2026-09-19 12:01:59 -0400

    created animations

--- Assets/Scripts/TitleScreen/TitleScreen.cs
+++ Assets/Scripts/TitleScreen/TitleScreen.cs
@@ -1,16 +1,14 @@
+using TMPro;
 using UnityEngine;
 
 public class TitleScreen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public void DisplayCredits()
     {
-        
+
     }
+    
 }
