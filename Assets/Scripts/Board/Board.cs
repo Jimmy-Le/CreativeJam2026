009465f9d7eaa47c15b7983e7b66959eb7248814
jimmy <jimmy_le_@hotmail.com>
2026-09-20 16:34:36 -0400

    sound

--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -43,6 +43,7 @@ public class Board : MonoBehaviour
             {
                 GameUIScript.Instance.RestartLevel();
                 GameUIScript.Instance.DisplayStepsLeft();
+                GameUIScript.Instance.PlayMusic();
                 loadingScreen.SetActive(false);
                
             });
